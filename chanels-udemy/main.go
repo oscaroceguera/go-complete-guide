4ffdd8586d59aad3facfc7e6afaf199d45a1bdb1
oscaroceguera <oscaroceguera@gmail.com>
2024-04-26 16:59:07 -0700

    chanels

---
+++ chanels-udemy/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main(){
+	links := []string{
+		"http://google.com",
+		"http://amazon.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second)
+
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up")
+	c <- link
+}
